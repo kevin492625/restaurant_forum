cfd7fef48440a7cb239126ac3e688435d81e5be8
Cheng-Ze <kevin492625@gmail.com>
2018-07-06 16:18:09 +0800

    user on index page can select restaurants by category

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -3,7 +3,8 @@ class Admin::RestaurantsController < Admin::BaseController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
-    @restaurants = Restaurant.page(params[:page]).per(10)
+    @restaurants = Restaurant.page(params[:page]).per(9)
+    @categories = Category.all
   end
 
   def new
