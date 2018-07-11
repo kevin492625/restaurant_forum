cee731d64281514bf50f8662b1042dd66eda9c81
Cheng-Ze <kevin492625@gmail.com>
2018-07-11 18:48:17 +0800

    add user name to registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
 
   has_many :comments
